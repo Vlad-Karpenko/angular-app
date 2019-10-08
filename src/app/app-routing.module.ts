5227a857b5a0e64b1c5014dfb71bc12356598575
vs.karpenko <vs.karpenko@csltd.com.ua>
2019-10-08 16:45:53 +0300

    - add login and error component - add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ForbiddenPageComponent} from './shared/error-page/403/forbidden-page.component';
+import {NotFoundComponent} from './shared/error-page/404/not-found.component';
+import {ServerErrorComponent} from './shared/error-page/500/server-error.component';
+import {LoginComponent} from './login-page/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: '403',
+    component: ForbiddenPageComponent
+  },
+  {
+    path: '404',
+    component: NotFoundComponent
+  },
+  {
+    path: '500',
+    component: ServerErrorComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './shared/error-page/404/not-found.component';
+import { LoginComponent } from './login-page/login.component';
+import { ServerErrorComponent } from './shared/error-page/500/server-error.component';
+import { ForbiddenPageComponent } from './shared/error-page/403/forbidden-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    LoginComponent,
+    ServerErrorComponent,
+    ForbiddenPageComponent
   ],
   imports: [
     BrowserModule,
