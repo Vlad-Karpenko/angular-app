0582614ccba977b004fc91fa97520a247c2252e2
vs.karpenko <vs.karpenko@csltd.com.ua>
2019-10-09 12:44:30 +0300

    add header,footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,20 @@ import {CommonModule} from '@angular/common';
 import {ForbiddenPageComponent} from './error-page/403/forbidden-page.component';
 import {NotFoundComponent} from './error-page/404/not-found.component';
 import {ServerErrorComponent} from './error-page/500/server-error.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     ForbiddenPageComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
